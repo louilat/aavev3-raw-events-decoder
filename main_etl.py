f1aa7a99ff439f2c90166625e39001613ff0bb86
louilat <lmm.latournerie@gmail.com>
2025-02-23 13:31:16 +0100

    change snapshot day + add atoken decoding in main_etl

--- main_etl.py
+++ main_etl.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta, timezone
 import io
 import os
 from src.events_decoder.events_decoder import AaveV3RawEventsDecoder
+from src.events_decoder.token_transfer_decoder import AaveV3TokenTransferDecoder
 
 
 # Run parameters
@@ -11,12 +12,13 @@ output_path = None
 raw_events_input_path = None
 
 if raw_events_input_path is None:
-    yesterday = datetime.today() - timedelta(days=1)
-    yesterday = datetime(
-        yesterday.year, yesterday.month, yesterday.day, tzinfo=timezone.utc
+    snapshot_day = datetime.today() - timedelta(days=14)
+    snapshot_day = datetime(
+        snapshot_day.year, snapshot_day.month, snapshot_day.day, tzinfo=timezone.utc
     ).strftime("%Y-%m-%d")
-    raw_events_input_path = f"aave-raw-datasource/daily-raw-events/raw_events_snapshot_date={yesterday}/raw_events.json"
-    output_path = f"aave-raw-datasource/daily-decoded-events/decoded_events_snapshot_date={yesterday}/"
+    raw_events_input_path = f"aave-raw-datasource/daily-raw-events/raw_events_snapshot_date={snapshot_day}/raw_events.json"
+    atoken_events_input_path = f"aave-raw-datasource/daily-raw-events/raw_events_snapshot_date={snapshot_day}/raw_BalanceTransfer_events.json"
+    output_path = f"aave-raw-datasource/daily-decoded-events/decoded_events_snapshot_date={snapshot_day}/"
 
 
 AWS_ACCESS_KEY = os.environ["AWS_ACCESS_KEY"]
@@ -27,14 +29,15 @@ VERIFY = False
 
 print(f"Starting ETL...")
 
-print("Step 0: Fetch Pool contract abi and initializing decoder...")
+print("Step 0: Fetch Pool contract abi and initializing decoders...")
 
 with open("src/abi/aavev3_mainnet.json") as file:
     pool_contract_abi = json.load(file)
 
 decoder = AaveV3RawEventsDecoder(contract_abi=pool_contract_abi)
+atoken_decoder = AaveV3TokenTransferDecoder()
 
-print("Step 1: Fetch raw events data...")
+print("Step 1: Fetching raw events data...")
 
 ressource_s3 = boto3.resource(
     "s3",
@@ -44,6 +47,8 @@ ressource_s3 = boto3.resource(
     verify=VERIFY,
 )
 
+print("   --> Pool Raw events")
+
 obj = ressource_s3.Object(
     BUCKET,
     raw_events_input_path,
@@ -51,6 +56,17 @@ obj = ressource_s3.Object(
 data = obj.get()["Body"].read().decode("utf-8")
 json_data = json.loads(data)
 
+print("   --> AToken BalanceTransfer() events")
+
+obj = ressource_s3.Object(
+    BUCKET,
+    atoken_events_input_path,
+)
+data = obj.get()["Body"].read().decode("utf-8")
+json_atoken_data = json.loads(data)
+atoken_events = []
+for list_events in json_atoken_data:
+    atoken_events.extend(list_events)
 
 print("Step 2: Fetching events signatures from abi...")
 
@@ -64,9 +80,17 @@ print("Step 4: Decoding raw events...")
 
 decoder.decode_raw_events()
 
-print("Step 5: Finding all active users...")
+print("Step 5: Finding all active users in pool...")
 
 decoder.get_all_active_users(verbose=True)
+
+print("STEP 7: Decoding AToken BalanceTransfer() events...")
+
+atoken_decoder.decode_transfer_events(atoken_events)
+
+print("STEP 8: Finding all users active in AToken BalanceTransfers...")
+
+atoken_decoder.get_all_token_transfer_users()
 
 print("Step 6: Uploading files to s3...")
 
@@ -78,6 +102,8 @@ client_s3 = boto3.client(
     verify=VERIFY,
 )
 
+print("   --> Upload Pool events...")
+
 for event_name, output_table in decoder.all_decoded_events_dict.items():
     buffer = io.StringIO()
     output_table.to_csv(buffer, index=False)
@@ -87,12 +113,34 @@ for event_name, output_table in decoder.all_decoded_events_dict.items():
         Key=output_path + f"decoded_{event_name}.csv",
     )
 
+print("   --> Upload Pool active users...")
+
 buffer = io.StringIO()
 decoder.all_active_users.to_csv(buffer, index=False)
 client_s3.put_object(
     Body=buffer.getvalue(),
     Bucket=BUCKET,
     Key=output_path + f"all_active_users.csv",
+)
+
+print("   --> Upload AToken Transfer events...")
+
+buffer = io.StringIO()
+atoken_decoder.all_decoded_events.to_csv(buffer, index=False)
+client_s3.put_object(
+    Body=buffer.getvalue(),
+    Bucket=BUCKET,
+    Key=output_path + f"decoded_transfers.csv",
+)
+
+print("   --> Upload AToken Transfer users...")
+
+buffer = io.StringIO()
+atoken_decoder.all_active_users.to_csv(buffer, index=False)
+client_s3.put_object(
+    Body=buffer.getvalue(),
+    Bucket=BUCKET,
+    Key=output_path + f"all_atoken_transfer_users.csv",
 )
 
 print(f"Outputs successfully generated at {output_path}")
--- src/events_decoder/token_transfer_decoder.py
+++ src/events_decoder/token_transfer_decoder.py
@@ -1,3 +1,5 @@
+"""Class for decoding AToken BalanceTransfer() events"""
+
 from web3 import Web3
 import pandas as pd
 from pandas import DataFrame
@@ -9,6 +11,19 @@ class AaveV3TokenTransferDecoder:
         self.all_active_users: DataFrame = DataFrame()
 
     def decode_transfer_events(self, all_encoded_events: list) -> DataFrame:
+        """
+        Decodes a list of encoded event and reshapes then as a DataFrame with columns
+            - blockNumber: The block number of the event
+            - reserve: The reserve corresponding to the AToken being transfered
+            - from: The address of the sender
+            - to: The address of the receiver
+            - amount: The amount (scaled) being transfered
+
+        Args:
+            - all_encoded_events (list): The list of encoded events to decode
+        Returns:
+            - DataFrame: The dataframe with the decoded events data.
+        """
         decoded_events = []
         for encoded_event in all_encoded_events:
             decoded_events.append(self._decode_transfer(eval(encoded_event)))
@@ -21,13 +36,32 @@ class AaveV3TokenTransferDecoder:
         return self.all_decoded_events
 
     def get_all_token_transfer_users(self) -> DataFrame:
+        """
+        Gives the list of all users that where involved in a AToken BalanceTransfer() event
+        (`from` or `to`)
+
+        Returns:
+            - DataFrame: The dataframe containing the list of users involved in at least one
+                AToken Transfer() event.
+        """
         from_ = self.all_decoded_events["from"].tolist()
         to_ = self.all_decoded_events["to"].tolist()
         all_users = DataFrame({"active_user_address": from_ + to_}).drop_duplicates()
         self.all_active_users = all_users
         return all_users
 
-    def _decode_transfer(self, transfer_encoded_event):
+    def _decode_transfer(self, transfer_encoded_event: dict) -> dict:
+        """
+        Decodes the following events, coming from the AToken contracts:
+            - BalanceTransfer (index_topic_1 address from, index_topic_2 address to, uint256 value, uint256 index)
+            - Transfer (index_topic_1 address from, index_topic_2 address to, uint256 value)
+        Note that it does not decode the `index` field for BalanceTransfer
+
+        Args:
+            - transfer_decoded_event (dict): The encoded event
+        Returns:
+            - dict: The corresponding decoded event
+        """
         blockNumber = transfer_encoded_event["blockNumber"]
         reserve = transfer_encoded_event["reserve"]
         from_ = Web3.to_checksum_address(transfer_encoded_event["topics"][1][-40:])
